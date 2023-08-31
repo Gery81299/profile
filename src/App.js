24dba12a94176f897cd5905b6e62064068bfb4e5
gerardo <gerardo.jurado1@unmsm.edu.pe>
2023-08-31 16:30:53 -0500

    perfil de usuario

--- src/App.js
+++ src/App.js
@@ -1,24 +1,398 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  /*return (
+    <section style={{ backgroundColor: "#eee" }}>
+      <div className="container py-5">
+        <div className="row">
+          <div className="col">
+            <nav
+              aria-label="breadcrumb"
+              className="bg-light rounded-3 p-3 mb-4"
+            >
+              <ol className="breadcrumb mb-0">
+                <li className="breadcrumb-item">
+                  <a href="#">Home</a>
+                </li>
+                <li className="breadcrumb-item">
+                  <a href="#">User</a>
+                </li>
+                <li className="breadcrumb-item active" aria-current="page">
+                  User Profile
+                </li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-4">
+            <div className="card mb-4">
+              <div className="card-body text-center">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                  alt="avatar"
+                  className="rounded-circle img-fluid"
+                  style={{ width: "150px" }}
+                />
+                <h5 className="my-3">John Smith</h5>
+                <p className="text-muted mb-1">Full Stack Developer</p>
+                <p className="text-muted mb-4">Bay Area, San Francisco, CA</p>
+                <div className="d-flex justify-content-center mb-2">
+                  <button type="button" className="btn btn-primary">
+                    Follow
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary ms-1"
+                  >
+                    Message
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="card mb-4 mb-lg-0">
+              <div className="card-body p-0">
+                <ul className="list-group list-group-flush rounded-3">
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i className="fas fa-globe fa-lg text-warning"></i>
+                    <p className="mb-0">https://mdbootstrap.com</p>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i
+                      className="fab fa-github fa-lg"
+                      style={{ color: "#333333" }}
+                    ></i>
+                    <p className="mb-0">mdbootstrap</p>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i
+                      className="fab fa-twitter fa-lg"
+                      style={{ color: "#55acee" }}
+                    ></i>
+                    <p className="mb-0">@mdbootstrap</p>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i
+                      className="fab fa-instagram fa-lg"
+                      style={{ color: "#ac2bac" }}
+                    ></i>
+                    <p className="mb-0">mdbootstrap</p>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i
+                      className="fab fa-facebook-f fa-lg"
+                      style={{ color: "#3b5998" }}
+                    ></i>
+                    <p className="mb-0">mdbootstrap</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-8">
+          <div className="card mb-4">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-sm-3">
+                  <p className="mb-0">Full Name</p>
+                </div>
+                <div className="col-sm-9">
+                  <p className="text-muted mb-0">Johnatan Smith</p>
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <p className="mb-0">Email</p>
+                </div>
+                <div className="col-sm-9">
+                  <p className="text-muted mb-0">example@example.com</p>
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <p className="mb-0">Phone</p>
+                </div>
+                <div className="col-sm-9">
+                  <p className="text-muted mb-0">(097) 234-5678</p>
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <p className="mb-0">Mobile</p>
+                </div>
+                <div className="col-sm-9">
+                  <p className="text-muted mb-0">(098) 765-4321</p>
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <p className="mb-0">Address</p>
+                </div>
+                <div className="col-sm-9">
+                  <p className="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card mb-4 mb-md-0">
+              <div className="card-body">
+                <p className="mb-4">
+                  <span className="text-primary font-italic me-1">
+                    Assignment
+                  </span>{" "}
+                  Project Status
+                </p>
+                <p className="mb-1" style={{ fontSize: ".77rem" }}>
+                  Web Design
+                </p>
+                <div className="progress rounded" style={{ height: "5px" }}>
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "80%" }}
+                    aria-valuenow="80"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+                <p className="mt-4 mb-1" style={{ fontSize: ".77rem" }}>
+                  Website Markup
+                </p>
+                <div className="progress rounded" style={{ height: "5px" }}>
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "72%" }}
+                    aria-valuenow="72"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+                <p className="mt-4 mb-1" style={{ fontSize: ".77rem" }}>
+                  One Page
+                </p>
+                <div className="progress rounded" style={{ height: "5px" }}>
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "89%" }}
+                    aria-valuenow="89"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+                <p className="mt-4 mb-1" style={{ fontSize: ".77rem" }}>
+                  Mobile Template
+                </p>
+                <div className="progress rounded" style={{ height: "5px" }}>
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "55%" }}
+                    aria-valuenow="55"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+                <p className="mt-4 mb-1" style={{ fontSize: ".77rem" }}>
+                  Backend API
+                </p>
+                <div
+                  className="progress rounded mb-2"
+                  style={{ height: "5px" }}
+                >
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "66%" }}
+                    aria-valuenow="66"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card mb-4 mb-md-0">
+              <div className="card-body">
+                <p className="mb-4">
+                  <span className="text-primary font-italic me-1">
+                    Assignment
+                  </span>{" "}
+                  Project Status
+                </p>
+                <p className="mb-1" style={{ fontSize: ".77rem" }}>
+                  Web Design
+                </p>
+                <div className="progress rounded" style={{ height: "5px" }}>
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "80%" }}
+                    aria-valuenow="80"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+                <p className="mt-4 mb-1" style={{ fontSize: ".77rem" }}>
+                  Website Markup
+                </p>
+                <div className="progress rounded" style={{ height: "5px" }}>
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "72%" }}
+                    aria-valuenow="72"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+                <p className="mt-4 mb-1" style={{ fontSize: ".77rem" }}>
+                  One Page
+                </p>
+                <div className="progress rounded" style={{ height: "5px" }}>
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "89%" }}
+                    aria-valuenow="89"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+                <p className="mt-4 mb-1" style={{ fontSize: ".77rem" }}>
+                  Mobile Template
+                </p>
+                <div className="progress rounded" style={{ height: "5px" }}>
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "55%" }}
+                    aria-valuenow="55"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+                <p className="mt-4 mb-1" style={{ fontSize: ".77rem" }}>
+                  Backend API
+                </p>
+                <div
+                  className="progress rounded mb-2"
+                  style={{ height: "5px" }}
+                >
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: "66%" }}
+                    aria-valuenow="66"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );*/
+
+  return(
+    <div class="container-xl px-4 mt-4">
+    
+    <nav class="nav nav-borders">
+        <a class="nav-link active ms-0" href="https://www.bootdey.com/snippets/view/bs5-edit-profile-account-details" target="__blank">Perfil</a>
+        <a class="nav-link" href="https://www.bootdey.com/snippets/view/bs5-profile-billing-page" target="__blank">Ajustes</a>
+        <a class="nav-link" href="https://www.bootdey.com/snippets/view/bs5-profile-security-page" target="__blank">Seguridad</a>
+        <a class="nav-link" href="https://www.bootdey.com/snippets/view/bs5-edit-notifications-page"  target="__blank">Notificaciones</a>
+    </nav>
+    <hr class="mt-0 mb-4"/>
+    <div class="row">
+        <div class="col-xl-4">
+            
+            <div class="card mb-4 mb-xl-0">
+                <div class="card-header">Profile Picture</div>
+                <div class="card-body text-center">
+                    
+                    <img class="img-account-profile rounded-circle mb-2" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt=""/>
+                    
+                    <div class="small font-italic text-muted mb-4">Gerardo Jurado</div>
+                 
+                    <button class="btn btn-primary" type="button">Contactar</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-8">
+            
+            <div class="card mb-4">
+                <div class="card-header">Account Details</div>
+                <div class="card-body">
+                    <form>
+                        
+                        <div class="mb-3">
+                            <label class="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
+                            <input class="form-control" id="inputUsername" type="text" placeholder="Enter your username" value="username"/>
+                        </div>
+                        
+                        <div class="row gx-3 mb-3">
+                            
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputFirstName">First name</label>
+                                <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" value="Valerie"/>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputLastName">Last name</label>
+                                <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna"/>
+                            </div>
+                        </div>
+                        
+                        <div class="row gx-3 mb-3">
+                            
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputOrgName">Organization name</label>
+                                <input class="form-control" id="inputOrgName" type="text" placeholder="Enter your organization name" value="Start Bootstrap"/>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputLocation">Location</label>
+                                <input class="form-control" id="inputLocation" type="text" placeholder="Enter your location" value="San Francisco, CA"/>
+                            </div>
+                        </div>
+                       
+                        
+                       
+                        <div class="row gx-3 mb-3">
+                            
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputPhone">Phone number</label>
+                                <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="555-123-4567"/>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputBirthday">Birthday</label>
+                                <input class="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Enter your birthday" value="06/10/1988"/>
+                            </div>
+                        </div>
+                        
+                        <button class="btn btn-primary" type="button">Guardar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
+
   );
 }
 
